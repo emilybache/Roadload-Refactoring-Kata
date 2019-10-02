d0b57bd839d0d3a5caf02c3ad1f9d5a9e0cd9b7c
Steffen Tøsse Brun-Michalsen <steffen.tosse.brun@if.no>
2019-10-02 14:29:05 +0200

    Update to .NET Core
    Update to .NET Core 3.0, .NET Standard and xUnit

---
+++ csharp/Roadload/Co2Values.cs
@@ -0,0 +1,26 @@
+﻿namespace Roadload
+{
+    public abstract class Co2Values
+    {
+        public double? HighRoadloadF0 { get; set; }
+        public double? HighRoadloadF1 { get; set; }
+        public double? HighRoadloadF2 { get; set; }
+        public double? HighRoadloadTm { get; set; }
+        public double? HighRoadloadCombined { get; set; }
+
+        public double? MidRoadloadF0 { get; set; }
+        public double? MidRoadloadF1 { get; set; }
+        public double? MidRoadloadF2 { get; set; }
+        public double? MidRoadloadTm { get; set; }
+        public double? MidRoadloadCombined { get; set; }
+
+        public double? LowRoadloadF0 { get; set; }
+        public double? LowRoadloadF1 { get; set; }
+        public double? LowRoadloadF2 { get; set; }
+        public double? LowRoadloadTm { get; set; }
+        public double? LowRoadloadCombined { get; set; }
+
+        public bool IsMidPoint { get; set; }
+        public bool IsSinglePoint { get; set; }
+    }
+}
