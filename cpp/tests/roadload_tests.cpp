7b8314787cb1f63abe84bc305cecf854b41adfa5
emily <emily@bacheconsulting.com>
2020-10-13 14:44:37 +0200

    first test

--- cpp/tests/roadload_tests.cpp
+++ cpp/tests/roadload_tests.cpp
@@ -10,9 +10,31 @@ extern "C"
 #include "roadload.h"
 }
 
-TEST_CASE ("foobar") {
+TEST_CASE ("calculateEnergyWithLoads") {
 
-    const CO2Values *co2Values = new CO2Values();
-    int result = calculateEnergyWithLoads(co2Values);
-    REQUIRE(result != 0);
+    // These are the sample test data values listed in the README file
+    auto *co2Values = new CO2Values();
+    co2Values->High_roadload_f0 = new CO2Value{.value=3.5, .hasValue=true};
+    co2Values->High_roadload_f1 = new CO2Value{.value=10.6575, .hasValue=true};
+    co2Values->High_roadload_f2 = new CO2Value{.value=6.7, .hasValue=true};
+    co2Values->High_roadload_tm = new CO2Value{.value=0.003, .hasValue=true};
+    co2Values->Mid_roadload_f0 = new CO2Value{.value=3.2, .hasValue=true};
+    co2Values->Mid_roadload_f1 = new CO2Value{.value=9.75, .hasValue=true};
+    co2Values->Mid_roadload_f2 = new CO2Value{.value=5.9, .hasValue=true};
+    co2Values->Mid_roadload_tm = new CO2Value{.value=0.0025, .hasValue=true};
+    co2Values->Low_roadload_f0 = new CO2Value{.value=3.1, .hasValue=true};
+    co2Values->Low_roadload_f1 = new CO2Value{.value=8.45, .hasValue=true};
+    co2Values->Low_roadload_f2 = new CO2Value{.value=5.3, .hasValue=true};
+    co2Values->Low_roadload_tm = new CO2Value{.value=0.002, .hasValue=true};
+
+    // These are the values we will check afterwards
+    co2Values->Low_roadload_combined = new CO2Value{.value=0, .hasValue=false};
+    co2Values->Mid_roadload_combined = new CO2Value{.value=0, .hasValue=false};
+    co2Values->High_roadload_combined = new CO2Value{.value=0, .hasValue=false};
+
+    calculateEnergyWithLoads(co2Values);
+
+    CHECK(co2Values->Low_roadload_combined->value == Approx(0.0004222231).margin(1e-10));
+    CHECK(co2Values->Mid_roadload_combined->value == Approx(0.0).margin(1e-10));
+    CHECK(co2Values->High_roadload_combined->value == Approx(0.0007987579).margin(1e-10));
 }
