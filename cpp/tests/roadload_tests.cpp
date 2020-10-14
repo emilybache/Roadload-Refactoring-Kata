c3c1e4c35db7a77064d2130cc5690339bcdca2ef
emily <emily@bacheconsulting.com>
2020-10-14 17:10:00 +0200

    with full coverage

--- cpp/tests/roadload_tests.cpp
+++ cpp/tests/roadload_tests.cpp
@@ -12,19 +12,39 @@ extern "C"
 #include "roadload.h"
 }
 
-TEST_CASE ("calculateEnergyWithLoads") {
+static CO2Values *
+doCalculateEnergyWithLoads(bool hasHighRoadloadF0, bool hasMidRoadloadF0, bool hasLowRoadloadF0, bool isSinglePoint,
+                           bool isMidPoint);
 
-    // These are the sample test data values listed in the README file
+TEST_CASE ("calculateEnergyWithLoads")
+{
+    std::vector<bool> options = {true, false};
+    ApprovalTests::CombinationApprovals::verifyAllCombinations(doCalculateEnergyWithLoads,
+                                                               options,
+                                                               options,
+                                                               options,
+                                                               options,
+                                                               options
+                                                               );
+}
+
+static CO2Values *
+doCalculateEnergyWithLoads(bool hasHighRoadloadF0,
+                           bool hasMidRoadloadF0,
+                           bool hasLowRoadloadF0,
+                           bool isSinglePoint,
+                           bool isMidPoint) {
     auto *co2Values = new CO2Values();
-    co2Values->High_roadload_f0 = new CO2Value{.value=3.5, .hasValue=true};
+    // These are the sample test data values listed in the README file
+    co2Values->High_roadload_f0 = new CO2Value{.value=3.5, .hasValue=hasHighRoadloadF0};
     co2Values->High_roadload_f1 = new CO2Value{.value=10.6575, .hasValue=true};
     co2Values->High_roadload_f2 = new CO2Value{.value=6.7, .hasValue=true};
     co2Values->High_roadload_tm = new CO2Value{.value=0.003, .hasValue=true};
-    co2Values->Mid_roadload_f0 = new CO2Value{.value=3.2, .hasValue=true};
+    co2Values->Mid_roadload_f0 = new CO2Value{.value=3.2, .hasValue=hasMidRoadloadF0};
     co2Values->Mid_roadload_f1 = new CO2Value{.value=9.75, .hasValue=true};
     co2Values->Mid_roadload_f2 = new CO2Value{.value=5.9, .hasValue=true};
     co2Values->Mid_roadload_tm = new CO2Value{.value=0.0025, .hasValue=true};
-    co2Values->Low_roadload_f0 = new CO2Value{.value=3.1, .hasValue=true};
+    co2Values->Low_roadload_f0 = new CO2Value{.value=3.1, .hasValue=hasLowRoadloadF0};
     co2Values->Low_roadload_f1 = new CO2Value{.value=8.45, .hasValue=true};
     co2Values->Low_roadload_f2 = new CO2Value{.value=5.3, .hasValue=true};
     co2Values->Low_roadload_tm = new CO2Value{.value=0.002, .hasValue=true};
@@ -34,7 +54,10 @@ TEST_CASE ("calculateEnergyWithLoads") {
     co2Values->Mid_roadload_combined = new CO2Value{.value=0, .hasValue=false};
     co2Values->High_roadload_combined = new CO2Value{.value=0, .hasValue=false};
 
-    calculateEnergyWithLoads(co2Values);
+    // These are the values to vary
+    co2Values->IsSinglePoint = isSinglePoint;
+    co2Values->IsMidPoint = isMidPoint;
 
-    ApprovalTests::Approvals::verify(co2Values);
+    calculateEnergyWithLoads(co2Values);
+    return co2Values;
 }
