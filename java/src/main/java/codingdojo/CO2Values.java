11f77459c7982939aa85789bafc3ec2a07ac1c7b
emily <emily@bacheconsulting.com>
2021-09-17 16:23:11 +0200

    first draft of java version

---
+++ java/src/main/java/codingdojo/CO2Values.java
@@ -0,0 +1,24 @@
+package codingdojo;
+
+public class CO2Values {
+    public Double High_roadload_f0;
+    public Double High_roadload_f1;
+    public Double High_roadload_f2;
+    public Double High_roadload_tm;
+    public Double High_roadload_combined;
+
+    public Double Mid_roadload_f0;
+    public Double Mid_roadload_f1;
+    public Double Mid_roadload_f2;
+    public Double Mid_roadload_tm;
+    public Double Mid_roadload_combined;
+
+    public Double Low_roadload_f0;
+    public Double Low_roadload_f1;
+    public Double Low_roadload_f2;
+    public Double Low_roadload_tm;
+    public Double Low_roadload_combined;
+
+    public Boolean IsMidPoint;
+    public Boolean IsSinglePoint;
+}
